0d38e3c021e41afaff9cf7b4ce0e5b94c846a676
pozercpp <n.a.chigrin@yandex.ru>
2025-11-17 01:12:47 +0300

    Update destructor

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -1,22 +1,18 @@
-#include<allocator.h>
+#include <allocator.h>
 
-Allocator::Allocator(size_t size_) : size(size_) {
-    if (size == 0) {
-        throw std::invalid_argument("Pool size must be greater than zero");
-    }
-    buffer = static_cast<char*>(::operator new(size));
-    if (!buffer) {
-        throw std::bad_alloc();
-    }
+Allocator::Allocator(size_t pool_size) : size(pool_size) {
+    if (pool_size == 0) { throw std::invalid_argument("Pool size must be greater than zero"); }
+    buffer = static_cast<char*>(::operator new(pool_size));
+    if (!buffer) { throw std::bad_alloc(); }
 }
 
-Allocator::~Allocator() noexcept { ::operator delete(buffer); }
+Allocator::~Allocator() noexcept {
+    allocated_blocks.clear();
+    ::operator delete(buffer);
+}
 
 void* Allocator::do_allocate(size_t bytes, size_t alignment) {
     if (bytes == 0) { return nullptr; }
-    allocated_blocks.sort([](const auto& a, const auto& b) {
-        return a.first < b.first;
-    });
     char* start = buffer;
     char* end = buffer + size;
     for (auto it = allocated_blocks.begin(); it != allocated_blocks.end(); ++it) {
@@ -25,7 +21,9 @@ void* Allocator::do_allocate(size_t bytes, size_t alignment) {
             void* aligned_ptr = start;
             size_t space = gap_end - start;
             if (std::align(alignment, bytes, aligned_ptr, space)) {
-                allocated_blocks.insert(it, {aligned_ptr, bytes});
+                auto insert_it = allocated_blocks.begin();
+                while (insert_it != allocated_blocks.end() && insert_it->first < aligned_ptr) { ++insert_it; }
+                allocated_blocks.insert(insert_it, {aligned_ptr, bytes});
                 return aligned_ptr;
             }
         }
@@ -44,13 +42,8 @@ void* Allocator::do_allocate(size_t bytes, size_t alignment) {
 
 void Allocator::do_deallocate(void* p, size_t bytes, size_t alignment) {
     if (p == nullptr) { return; }
-    auto it = std::find_if(allocated_blocks.begin(), allocated_blocks.end(),
-                           [p, bytes](const auto& block) {
-                               return block.first == p && block.second == bytes;
-                           });
-    if (it != allocated_blocks.end()) {
-        allocated_blocks.erase(it);
-    }
+    auto it = find(allocated_blocks.begin(), allocated_blocks.end(), std::make_pair(p, bytes));
+    if (it != allocated_blocks.end()) { allocated_blocks.erase(it); }
 }
 
 bool Allocator::do_is_equal(const std::pmr::memory_resource& other) const noexcept { return this == &other; }
