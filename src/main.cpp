8615c170182480a9960fc17dcf18baf8b1c13920
pozercpp <n.a.chigrin@yandex.ru>
2025-11-19 14:57:29 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,16 @@ int main() {
     for (auto& i : arr) {
         std::cout << i << " ";
     }
+    std::reverse(arr.begin(), arr.end());
+    std::cout << std::endl;
+    for (auto& i : arr) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    std::sort(arr.begin(), arr.end());
+    for (auto& i : arr) {
+        std::cout << i << " ";
+    }
     std::cout << std::endl;
     return 0;
 }
